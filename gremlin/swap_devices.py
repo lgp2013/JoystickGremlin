cb1fe4aac894a78f944924c4e20be56f08ee3d33
WhiteMagic <white.magic@gmx.de>
2026-02-14 18:26:06 +0100

    Device swap input item counting fixes and cleanups

--- gremlin/swap_devices.py
+++ gremlin/swap_devices.py
@@ -10,29 +10,50 @@ import dataclasses
 import uuid
 
 import gremlin.profile
-from gremlin import device_initialization
 
 
 @dataclasses.dataclass
 class ProfileDeviceInfo:
+
     device_uuid: uuid.UUID
     name: str = ""
     num_bindings: int = 0
 
 
+@dataclasses.dataclass
+class SwapDevicesResult:
+
+    action_swaps: int = 0
+    input_swaps: int = 0
+    user_script_swaps: int = 0
+
+    def as_string(self) -> str:
+        return (
+            f"Swapped {self.input_swaps} input(s), {self.action_swaps} "
+            f"actions, and {self.user_script_swaps} user script variables."
+        )
+
+
 def get_profile_devices(
     profile: gremlin.profile.Profile
 ) -> list[ProfileDeviceInfo]:
-    # Retrieve all devices that have input bindings in the profile and count
-    # the number of bindings for each.
+    """Returns ProfileDeviceInfo for all devices present in the profile.
+
+    Args:
+        profile: The Profile to analyze.
+
+    Returns:
+        A list of ProfileDeviceInfo objects for all devices present in the
+        profile.
+    """
+    # Count the number of non-empty bindings.
     profile_devices = {}
     for device_uuid, inputs in profile.inputs.items():
-        if device_uuid in profile_devices:
-            profile_devices[device_uuid].num_bindings += len(inputs)
-        else:
-            profile_devices[device_uuid] = ProfileDeviceInfo(
-                device_uuid, num_bindings=len(inputs)
-            )
+        if device_uuid not in profile_devices:
+            profile_devices[device_uuid] = ProfileDeviceInfo(device_uuid)
+
+        binding_count = sum(1 for e in inputs if e.action_sequences)
+        profile_devices[device_uuid].num_bindings += binding_count
 
     # Attempt to retrieve device name from the database.
     for dev_info in profile.device_database.devices.values():
@@ -42,38 +63,23 @@ def get_profile_devices(
     return list(profile_devices.values())
 
 
-@dataclasses.dataclass
-class SwapDevicesResult:
-    action_swaps: int
-    input_swaps: int
-    user_script_swaps: int
-
-    def as_string(self) -> str:
-        return (
-            f"Swapped {self.action_swaps} actions, "
-            f"{self.input_swaps} inputs, and {self.user_script_swaps} user script vars."
-        )
-
-
 def _swap_device_inputs(
     profile: gremlin.profile.Profile,
     source_device_uuid: uuid.UUID,
     target_device_uuid: uuid.UUID,
 ) -> int:
     swap_count = 0
-    source_device_inputs = profile.inputs.get(source_device_uuid)
-    target_device_inputs = profile.inputs.get(target_device_uuid)
+    source_device_inputs = profile.inputs.get(source_device_uuid, [])
+    target_device_inputs = profile.inputs.get(target_device_uuid, [])
 
-    if source_device_inputs:
-        for input_item in source_device_inputs:
-            input_item.device_id = target_device_uuid
-            swap_count += 1
-        profile.inputs[target_device_uuid] = source_device_inputs
-    if target_device_inputs:
-        for input_item in target_device_inputs:
-            input_item.device_id = source_device_uuid
-            swap_count += 1
-        profile.inputs[source_device_uuid] = target_device_inputs
+    for input_item in [e for e in source_device_inputs if e.action_sequences]:
+        input_item.device_id = target_device_uuid
+        swap_count += 1
+    profile.inputs[target_device_uuid] = source_device_inputs
+    for input_item in [e for e in target_device_inputs if e.action_sequences]:
+        input_item.device_id = source_device_uuid
+        swap_count += 1
+    profile.inputs[source_device_uuid] = target_device_inputs
     return swap_count
 
 
@@ -82,10 +88,10 @@ def _swap_device_actions(
     source_device_uuid: uuid.UUID,
     target_device_uuid: uuid.UUID,
 ) -> int:
-    swap_count = 0
-    for action in profile.library.actions_by_predicate(lambda x: True):
-        swap_count += int(action.swap_uuid(source_device_uuid, target_device_uuid))
-    return swap_count
+    return [
+        a.swap_uuid(source_device_uuid, target_device_uuid) for a in
+        profile.library.actions_by_predicate(lambda _: True)
+    ].count(True)
 
 
 def _swap_device_user_script_vars(
@@ -93,10 +99,10 @@ def _swap_device_user_script_vars(
     source_device_uuid: uuid.UUID,
     target_device_uuid: uuid.UUID,
 ) -> int:
-    swap_count = 0
-    for script in profile.scripts.scripts:
-        swap_count += script.swap_uuid(source_device_uuid, target_device_uuid)
-    return swap_count
+    return [
+        script.swap_uuid(source_device_uuid, target_device_uuid) for script in
+        profile.scripts.scripts
+    ].count(True)
 
 
 def swap_devices(
@@ -104,9 +110,11 @@ def swap_devices(
     source_device_uuid: uuid.UUID,
     target_device_uuid: uuid.UUID,
 ) -> SwapDevicesResult:
-    """Swaps two devices in the profile (from a device in the profile to a connected device).
+    """Swaps two devices in the profile, from a device in the profile to a
+    connected device.
 
-    It is the caller's responsibility to ensure that the devices UUIDs are valid.
+    It is the caller's responsibility to ensure that the devices UUIDs are
+    valid.
 
     Args:
         profile: The Profile to perform the swap on.
@@ -119,5 +127,9 @@ def swap_devices(
     return SwapDevicesResult(
         _swap_device_actions(profile, source_device_uuid, target_device_uuid),
         _swap_device_inputs(profile, source_device_uuid, target_device_uuid),
-        _swap_device_user_script_vars(profile, source_device_uuid, target_device_uuid),
+        _swap_device_user_script_vars(
+            profile,
+            source_device_uuid,
+            target_device_uuid
+        )
     )
